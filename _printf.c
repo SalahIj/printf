0c2b3e62f8f3bf265094a29bddd60dc1bd44a20d
zakaria1011 <grarizakaria@gmail.com>
2023-07-23 17:16:47 +0100

    zakaria

--- _printf.c
+++ _printf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 
 	va_start(ptr, format);
 	if (!format || (format[0] == '%' && !format[1]))
-		return (0);
+		return (-1);
 
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
