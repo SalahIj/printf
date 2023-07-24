e593c2a523532ce34742ee0f1586b31ec00e960f
zakaria1011 <grarizakaria@gmail.com>
2023-07-25 00:18:02 +0100

    zakaria

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int main(void) {
+int main() {
     int num_chars = _printf("Hello, %s! I am %c. This is a percent sign: %%.\n", "John", 'A');
     
     printf("Number of characters printed: %d\n", num_chars);
