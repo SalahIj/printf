680d5eda2e90d9df2883a38d386b5b0937350a95
zakaria1011 <grarizakaria@gmail.com>
2023-07-24 10:47:38 +0100

    zakaria1011

--- _printf_Percent.c
+++ _printf_Percent.c
@@ -3,7 +3,9 @@
  * _printf_Percent - print using _putchar
  * Return: result
 */
-int _printf_Percent(void)
+int _printf_Percent(va_list args)
 {
+char p;
+p = va_arg(args, int);
 return (_putchar(37));
 }
--- main.h
+++ main.h
@@ -11,7 +11,7 @@ int _printf_String(va_list ptr);
 int _printf(const char *format, ...);
 int _strlen(const char *s);
 int _printf_Character(va_list ptr);
-int _printf_Percent(void);
+int _printf_Percent(va_list args);
 int checker(const char *format);
 int end(const char *format);
 
@@ -25,7 +25,7 @@ int end(const char *format);
 typedef struct Option
 {
 	char car;
-	int (*f)();
+	int (*f)(va_list);
 } opt;
 
 #endif
