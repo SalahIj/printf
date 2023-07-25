c90bd9a97b711038c6728b5841f27ad2b1d23c01
zakaria1011 <grarizakaria@gmail.com>
2023-07-25 12:59:32 +0100

    zakaria1011

---
+++ _convert_Hexadicimal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _convert_Hexadicimal - convert to Hexa lower case
+ * @ptr: input
+ * Return: the hexa
+*/
+int _convert_Hexadicimal(va_list ptr)
+{
+int i, j;
+unsigned int p, m, *array, b;
+p = va_arg(ptr, unsigned int);
+m = p;
+for (i = 0; p != 0; i++, p = p / 16)
+;
+array = (unsigned int *)malloc(sizeof(unsigned int) * i);
+if (array == NULL)
+return (0);
+for (j = i - 1; j >= 0; j--, m /= 16)
+array[j] = m % 16;
+j++;
+while (j < i)
+{
+if (array[j] <= 9)
+_putchar(array[j] + '0');
+else
+{
+b = array[j] - 9;
+_putchar(b + 96);
+}
+j++;
+}
+free(array);
+return (i);
+}
