a74501855a65a8503ba26b9d26ac0f6112b73a40
ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-07-25 11:20:13 -0700

    SalahIj

--- _printf_Pointer.c
+++ _printf_Pointer.c
@@ -9,15 +9,15 @@
 int _printf_Pointer(va_list ptr)
 {
 	void *pointer;
-	char *str = "(null)";
+	char *str = "(nil)";
 	long int counter = 0;
 	unsigned long int pt;
 
 	pointer = va_arg(ptr, void*);
 	if (pointer == NULL)
 	{
-		write(1, str, 6);
-		return (6);
+		write(1, str, 5);
+		return (5);
 	}
 	else
 	{
