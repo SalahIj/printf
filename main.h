efa39d59d47a69dd98addf6506fe368d5a841770
zakaria1011 <grarizakaria@gmail.com>
2023-07-25 11:01:56 +0100

    zakaria1011

--- main.h
+++ main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 #include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int _putchar(const char c);
 int _printf_String(va_list ptr);
@@ -14,18 +15,13 @@ int _printf_Character(va_list ptr);
 int _printf_Percent(void);
 int specifier_Check(char cd, va_list ptr);
 int check_letters(char cd);
-
-/**
- * struct Option - struct name option.
- * @car: pointer.
- * @f: function pointer.
- * Description: info.
- */
+int _printf_Integer_Decimal(va_list args);
+void _printf_Digits(int n);
 
 typedef struct Option
 {
-	char car;
-	int (*f)();
+    char car;
+    int (*f)();
 } opt;
 
 #endif
--- specifier_Check.c
+++ specifier_Check.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * specifier_Check - checking specifier.
- * @cd: the first input.
- * @ptr: the second input.
- * Return: the result.
- */
+ * specifier_Check - check specifier letter
+ * @cd: char
+ * @ptr: va_list
+ * Return: counter
+*/
 
 int specifier_Check(char cd, va_list ptr)
 {
-	opt specifier[] = {{'c', _printf_Character}, {'s', _printf_String},
-				{'%', _printf_Percent}};
-	int counter = 0;
-	unsigned int j;
-
-	for (j = 0; j < sizeof(specifier) / sizeof(specifier[0]); j++)
-	{
-		if (cd == specifier[j].car)
-		{
-			counter = specifier[j].f(ptr);
-			break;
-		}
-	}
-	return (counter);
+opt specifier[] = {{'c', _printf_Character}, {'s', _printf_String},
+{'%', _printf_Percent}, {'d', _printf_Integer_Decimal},
+{'i', _printf_Integer_Decimal}};
+int counter = 0;
+unsigned int j;
+for (j = 0; j < sizeof(specifier) / sizeof(specifier[0]); j++)
+{
+if (cd == specifier[j].car)
+{
+counter = specifier[j].f(ptr);
+break;
+}
+}
+return (counter);
 }
