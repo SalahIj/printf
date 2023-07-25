538d74c0a961ad35a7bd1e9ebe8aa9d55530ce25
ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-07-25 04:07:18 -0700

    SalahIj

--- _printf_Integer_Decimal.c
+++ _printf_Integer_Decimal.c
@@ -1,51 +1,37 @@
 #include "main.h"
+
 /**
- * _printf_Digits - printf digit
- * @n: num
- * Return: integer
-*/
-int _printf_Digits(int n)
+ * _printf_Integer_Decimal - printing
+ * @ptr: the input.
+ * Return: the result.
+ */
+
+int _printf_Integer_Decimal(va_list ptr)
 {
-int ind = 0, p;
-int num, num_div, div = 1;
-num = n;
-for (num_div = num; num_div > 9; num_div /= 10)
-{
-div *= 10;
-ind++;
+	int num, div = 1, counter = 0, digit, num_div, p;
+
+	num = va_arg(ptr, int);
+	p = num;
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	for (num_div = num; num_div > 9; num_div /= 10)
+	{
+		div *= 10;
+	}
+	while (div > 0)
+	{
+		digit = num / div;
+		_putchar('0' + digit);
+		num %= div;
+		div /= 10;
+		counter++;
+	}
+	if (p >= 0)
+		return (counter);
+	else
+		return (counter + 1);
 }
-p = n;
-if (p != 0)
-{
-_printf_Digits(p / 10);
-_putchar('0' + (p % 10));
-p = p / 10;
-}
-return (ind + 1);
-}
-/**
- * _printf_Integer_Decimal - printf Integer
- * @args: va_list
- * Return: return lenght of num
-*/
-int _printf_Integer_Decimal(va_list args)
-{
-int p = va_arg(args, int);
-int pt, num;
-num = p;
-if (num < 0)
-{
-_putchar('-');
-num = -num;
-}
-if (num == 0)
-{
-_putchar('0');
-return (1);
-}
-pt = _printf_Digits(num);
-if (p > 0)
-return (pt);
-else
-return (pt + 1);
-}
+
