b0d2b571b81774a81f5019d40c5d2a9aa413c506
ijbar Salah Eddine <ijbarbusiness@gmail.com>
2023-07-25 18:42:06 -0700

    SalahIj

--- _printf.c
+++ _printf.c
@@ -5,10 +5,9 @@
  * @format: the input of the function.
  * Return: the result.
 */
-
 int _printf(const char *format, ...)
 {
-	int i = 0, counter = 0;
+	int i = 0, counter = 0, k;
 	va_list ptr;
 
 	va_start(ptr, format);
@@ -21,16 +20,28 @@ int _printf(const char *format, ...)
 			i++;
 			if (format[i] == '\0')
 				return (-1);
+			if (_flag_specifier_search(format[i], format[i + 1]) == 0)
+			{
+				if (format[i + 1] == ' ')
+				{
+					for (k = i + 1; format[k] != ' '; k++)
+					;
+					counter += _flag_specifier_indice(format[i], format[k + 1], ptr);
+					i = i + (k - i + 1);
+				}
+				else
+				{
+					counter += _flag_specifier_indice(format[i], format[i + 1], ptr);
+					i++;
+				}
+			}
 			if (check_letters(format[i]) == 0)
 				counter += 2;
 			else
 				counter += specifier_Check(format[i], ptr);
 		}
 		else
-		{
-			_putchar(format[i]);
-			counter++;
-		}
+			counter += _putchar(format[i]);
 		i++;
 	}
 	return (counter);
